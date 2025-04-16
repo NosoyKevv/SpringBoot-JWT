e487c063e15cb6eb6174586d7e96b5fb2899c917
Kevin Vergel <kevinvergel@wposs.com>
2025-04-16 10:09:06 -0500

    CommandLineRunner-> encriptar pass

--- src/main/java/com/SpringSecurityJWT/SpringSecurityJwtApplication.java
+++ src/main/java/com/SpringSecurityJWT/SpringSecurityJwtApplication.java
@@ -1,13 +1,32 @@
 package com.SpringSecurityJWT;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SpringSecurityJwtApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityJwtApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJwtApplication.class, args);
+    }
 
+    private final PasswordEncoder passwordEncoder;
+
+    public SpringSecurityJwtApplication(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    //todo 1:00:00 -> https://www.youtube.com/watch?v=735a83FQR2I
+    //todo Generar password para script import
+//    @Bean
+//    public CommandLineRunner createPasswordsCommand() {
+//        return args -> {
+//            System.out.println(passwordEncoder.encode("1007912636"));
+//            System.out.println(passwordEncoder.encode("60415654"));
+//        };
+//
+//    }
 }
