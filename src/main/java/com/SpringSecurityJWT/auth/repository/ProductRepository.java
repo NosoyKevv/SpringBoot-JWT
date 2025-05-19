e0a0a99af952df5755dba4b0cd5cf394ef570364
Kevin Vergel <kevinvergel@wposs.com>
2025-05-19 12:27:25 -0500

    New Branch.

--- src/main/java/com/SpringSecurityJWT/auth/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.SpringSecurityJWT.auth.repository;
-
-import com.SpringSecurityJWT.auth.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-
-}
